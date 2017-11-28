975ce0836bfb715818cebd96d250ed2f400f871e
Carlos Rodriguez Niama <carlos.rodriguezn@espoch.edu.ec>
2017-11-28 18:36:01 -0500

    ajuste

---
+++ SitioWebOasis/ProxySeguro/GestorDefinicionPeriodo.cs
@@ -0,0 +1,30 @@
+﻿using OAS_Seguridad.Cliente;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+
+namespace SitioWebOasis.ProxySeguro
+{
+    public class GestorDefinicionPeriodo : WSGestorDefinicionPeriodoCarrera.GestorDefinicionPeriodo
+    {
+        private OASisLogin login;
+
+        public GestorDefinicionPeriodo()
+        {
+            this.login = SitioWebOasis.CommonClasses.CacheConfig.Get("OASisLogin") as OASisLogin;
+        }
+
+        protected override WebRequest GetWebRequest(Uri uri)
+        {
+            HttpWebRequest req2 = (HttpWebRequest)base.GetWebRequest(uri);
+            if (login != null)
+            {
+                login.AttachCredentials(req2);
+            }
+
+            return req2;
+        }
+    }
+}
