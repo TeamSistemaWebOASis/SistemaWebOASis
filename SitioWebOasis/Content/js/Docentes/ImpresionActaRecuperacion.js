3afed31d34a50ffb434112789fa2da9e0b66f828
Carlos Rodriguez Niama <carlos.rodriguezn@espoch.edu.ec>
2018-01-09 11:27:59 -0500

    09ENE2018: gestion de mensajes de evaluaciones

---
+++ SitioWebOasis/Content/js/Docentes/ImpresionActaRecuperacion.js
@@ -0,0 +1,48 @@
+﻿$(document).ready(function () {
+
+    $('#pER_pdf, #pER_xls, #pER_blc').on('click', function () {
+        showLoadingProcess();
+
+        $.ajax({
+            type: "POST",
+            url: "/Docentes/impresionActas",
+            data: '{idActa: "' + $(this).attr("id") + '", idAsignatura: "' + $('#ddlLstPeriodosEstudiante').val() + '"}',
+            contentType: "application/json; charset=utf-8",
+            dataType: "json",
+            error: function (xhr, ajaxOptions, thrownError) {
+                console.log(xhr.responseText);
+
+                //  Mostrar mensaje de estado de la transaccion
+                getMensajeTransaccion(false, "Error, favor vuelva a intentarlo, si el problema persiste consulte en la secretaria de su carrara");
+
+                //  Cierro la ventana GIF Proceso
+                HoldOn.close();
+            }
+        }).complete(function (data) {
+            //  Oculto el mensaje de error
+            $('#messageError').attr("hidden");
+
+            //  Cierro la ventana GIF Proceso
+            HoldOn.close();
+
+            if (data.responseJSON.fileName != "none" && data.responseJSON.fileName != "") {
+                $.redirect("/Docentes/DownloadFile",
+                            { file: data.responseJSON.fileName },
+                                "POST")
+            } else {
+                //  Si existe error, muestro el mensaje
+                $('#messageError').removeAttr("hidden");
+                $('#messageError').html("<a href='' class='close'>×</a><strong>Problemas al generar archivo</strong>, favor vuelva a intentarlo");
+            }
+        })
+    })
+
+
+    function showLoadingProcess() {
+        HoldOn.open({
+            theme: 'sk-dot',
+            message: "<h4>PROCESANDO INFORMACIÓN ...</h4>"
+        });
+    }
+
+})
